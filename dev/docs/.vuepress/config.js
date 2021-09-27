235f235de4d8b64031332c0a8ab478315f8a6e7c
bgete <erictinadogcat@yahoo.com.tw>
2021-09-27 13:54:37 +0800

    test1

---
+++ dev/docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    base:'/vuepress/',
+    themeConfig: {
+        sidebar: [
+          '/',
+          '/Category_1/',
+          {
+              title:'Category_2_nest',
+              path:'/Category_2/',
+              children:[
+                  '/Category_2/test1.md',
+                  '/Category_2/test2.md'
+              ]
+          }
+          
+        ]
+      }
+  }
